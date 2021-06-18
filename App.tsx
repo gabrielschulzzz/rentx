8a13b39720dadd7dd15e59e476df77b8610e3b13
Gabriel <gabpaesschulzzz@gmail.com>
2021-06-18 16:40:49 -0300

    Tela de confirmação criada

--- App.tsx
+++ App.tsx
@@ -18,6 +18,8 @@ import { Home } from './src/screens/Home';
 import { CarDetails } from './src/screens/CarDetails'
 import { Scheduling } from './src/screens/Scheduling'
 import { SchedulingDetails } from './src/screens/SchedulingDetails'
+import { SchedulingComplete } from './src/screens/SchedulingComplete'
+
 
 import theme from './src/styles/theme';
 
@@ -36,7 +38,7 @@ export default function App() {
   
   return (
     <ThemeProvider theme={theme}>
-      <SchedulingDetails />
+      <SchedulingComplete />
     </ThemeProvider>
   );
 }
